3298a9cddf367df330c9ea188a084a51cb588ffb
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2025-12-12 22:56:22 +0000

    feat(logic): map color holder nodes to js variables

--- src/script.js
+++ src/script.js
@@ -4,3 +4,12 @@
  *  DATE-TIME WRITTEN: NOV 25, 2025; 9:33AM
  *  REPOSITORY: https://github.com/NelMatrix743/color_palette_generator.git
  */
+
+
+// map color holders dom nodes to js variables
+let colorHolder1 = document.getElementById("clh-0");
+let colorHolder2 = document.getElementById("clh-1");
+let colorHolder3 = document.getElementById("clh-2");
+let colorHolder4 = document.getElementById("clh-3");
+let colorHolder5 = document.getElementById("clh-4");
+
